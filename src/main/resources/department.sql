abd0a25e5ecae12995a602966562ae27f7a4e153
Mohammed Hasan <rupams2002@yahoo.com>
2021-08-31 16:04:56 -0400

    Initial push department service

---
+++ src/main/resources/department.sql
@@ -0,0 +1,10 @@
+--DROP TABLE IF EXISTS CITY;  
+--CREATE TABLE Department (  
+--	departmentId INT AUTO_INCREMENT  PRIMARY KEY,  
+--	departmentName VARCHAR(50) NOT NULL,  
+--	departmentAddress VARCHAR(50) NOT NULL,
+--	departmentCode VARCHAR(5) NOT NULL
+--);  
+
+--INSERT INTO `microservice`.`department` (`department_address`, `department_code`, `department_name`) 
+--VALUES (`IT-4`, `604 Gazebo Street`, `IT-004`);
